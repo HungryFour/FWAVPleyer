c2416caaffa26d9a9322e4c335a9002419b33f16
武建明 <wjmwjmwb@yeah.net>
2016-09-07 16:16:42 +0800

    Initial Commit

---
+++ FWAVPleyer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FWAVPleyer
+//
+//  Created by 武建明 on 16/9/7.
+//  Copyright © 2016年 Four_w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FWAVPleyer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FWAVPleyer
+//
+//  Created by 武建明 on 16/9/7.
+//  Copyright © 2016年 Four_w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
